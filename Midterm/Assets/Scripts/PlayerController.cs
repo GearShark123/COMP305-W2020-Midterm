acce31d9c8dd18e9296b40e84201bb21d7708205
Lyle <lylespurrell@hotmail.com>
2020-02-26 15:46:17 -0500

    Character movement script

---
+++ Midterm/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // "Public" variables
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float jumpForce = 700.0f;
+    [SerializeField] private Transform groundCheckPos;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private float groundCheckRadius = 0.15f;
+
+    // Private variables
+    private bool isGrounded = false;
+    private Rigidbody2D rBody;
+    private Animator anim;
+    private bool isFacingRight = true;
+    private bool isCrouching = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Physics
+    void FixedUpdate()
+    {
+        Vector2 movement = Vector2.zero;
+        float horiz = Input.GetAxis("Horizontal");
+
+        isGrounded = GroundCheck();
+
+        // Jumping code will go here!
+        if (isGrounded && Input.GetAxis("Jump") > 0)
+        {
+            rBody.AddForce(new Vector2(0.0f, jumpForce));
+            isGrounded = false;
+        }
+
+        rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
+
+
+        // Check if sprite is crouching
+        if (isGrounded && rBody.velocity.x == 0 && Input.GetAxis("Vertical") < 0)
+        {
+            isCrouching = true;
+        }
+        else
+        {
+            isCrouching = false;
+        }
+
+        anim.SetFloat("xSpeed", Mathf.Abs(rBody.velocity.x));
+        anim.SetFloat("ySpeed", rBody.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isCrouching", isCrouching);
+    }
+
+    private bool GroundCheck()
+    {
+        return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, whatIsGround); ;
+    }
+}
