0850cffd25d10aa433a4589d46bfe4026b45383a
Lyle <lylespurrell@hotmail.com>
2020-02-26 20:24:44 -0500

    Final treasure behaviour

---
+++ Midterm/Assets/Scripts/ChestController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestController : MonoBehaviour
+{
+    // "Public" variables
+    [SerializeField] private GameObject star;
+    [SerializeField] private Transform spawn;
+
+    // Private variables
+    private Animator anim;
+    private bool isOpening = false;
+    private bool isPlayer = false;
+    private float num = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (isPlayer == true && Input.GetKeyUp(KeyCode.E))
+        {
+            isOpening = true;
+            anim.SetBool("isOpening", isOpening);
+        }
+    }
+
+    // Physics
+    void FixedUpdate()
+    {
+        if (isOpening == true)
+        {
+            Invoke("Spawn", num);
+            num += 0.1f;
+        }
+    }
+
+    void Spawn()
+    {
+        Destroy(Instantiate(star, spawn.position, spawn.rotation), 3.0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            isPlayer = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            isPlayer = false;
+        }
+    }
+}
+
+
---
+++ Midterm/Assets/Scripts/StarController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour
+{
+    private Rigidbody2D rBody;
+    private Vector3 impulse = new Vector2(0.0f, 16.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        rBody.AddForce(impulse, ForceMode2D.Impulse);
+    }
+}
